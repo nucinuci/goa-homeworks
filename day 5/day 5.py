2bb13ca36eac87079f8edfde9b9096052174c467
nucinuci <imesashvilinutsa2019@gmail.com>
2024-02-19 20:02:36 +0400

    commit

---
+++ day 5/day 5.py
@@ -0,0 +1,20 @@
+
+num1 = int(input("Please enter  number: "))
+num2 = int(input("Please enter second number: "))
+
+
+#multiplication
+print(num1 * num2)
+
+#division
+print(num1 / num2)
+
+#addition
+print(num1 + num2)
+
+print(num1 > num2)
+
+#deduction
+print(num1 - num2)
+
+print(num1 < num2)
